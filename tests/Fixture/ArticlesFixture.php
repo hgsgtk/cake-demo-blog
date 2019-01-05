9e89e23cd8b0fc3d6c1f5da147984e1fb3e0fb0f
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-01-05 23:38:41 +0900

    modify test - specify fixture fields

--- tests/Fixture/ArticlesFixture.php
+++ tests/Fixture/ArticlesFixture.php
@@ -6,5 +6,36 @@ use Cake\TestSuite\Fixture\TestFixture;
 
 class ArticlesFixture extends TestFixture
 {
-    public $import = ['table' => 'articles'];
+    public $fields = [
+        'id' => [
+            'type' => 'integer'
+        ],
+        'user_id' => [
+            'type' => 'integer',
+        ],
+        'title' => [
+            'type' => 'string',
+            'length' => 255,
+            'null' => false,
+        ],
+        'slug' => [
+            'type' => 'string',
+            'length' => 191,
+            'null' => false,
+        ],
+        'body' => 'text',
+        'published' => [
+            'type' => 'integer',
+            'default' => '0',
+            'null' => false,
+        ],
+        'created' => 'datetime',
+        'modified' => 'datetime',
+        '_constraints' => [
+            'primary' => [
+                'type' => 'primary',
+                'columns' => ['id'],
+            ],
+        ],
+    ];
 }
