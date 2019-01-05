ef5d94a4a96ec1a67b869ecf843523419510405d
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-01-05 18:35:42 +0900

    fix cs

--- config/Migrations/20190105084049_Initial.php
+++ config/Migrations/20190105084049_Initial.php
@@ -1,153 +1,164 @@
 <?php
+
 use Migrations\AbstractMigration;
 
 class Initial extends AbstractMigration
 {
-	public function up()
-	{
-		$this->table('users')
-			->addColumn('email', 'string', [
-				'default' => null,
-				'limit' => 255,
-				'null' => false,
-			])
-			->addColumn('password', 'string', [
-				'default' => null,
-				'limit' => 255,
-				'null' => false,
-			])
-			->addColumn('created', 'datetime', [
-				'default' => null,
-				'limit' => null,
-				'null' => true,
-			])
-			->addColumn('modified', 'datetime', [
-				'default' => null,
-				'limit' => null,
-				'null' => true,
-			])
-			->create();
+    /**
+     * up
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->table('users')
+            ->addColumn('email', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
 
-		$this->table('articles')
-			->addColumn('user_id', 'integer', [
-				'default' => null,
-				'limit' => 11,
-				'null' => false,
-			])
-			->addColumn('title', 'string', [
-				'default' => null,
-				'limit' => 255,
-				'null' => false,
-			])
-			->addColumn('slug', 'string', [
-				'default' => null,
-				'limit' => 191,
-				'null' => false,
-			])
-			->addColumn('body', 'text', [
-				'default' => null,
-				'limit' => null,
-				'null' => true,
-			])
-			->addColumn('published', 'boolean', [
-				'default' => false,
-				'limit' => null,
-				'null' => true,
-			])
-			->addColumn('created', 'datetime', [
-				'default' => null,
-				'limit' => null,
-				'null' => false,
-			])
-			->addColumn('modified', 'datetime', [
-				'default' => null,
-				'limit' => null,
-				'null' => false,
-			])
-			->addIndex(['slug'], ['unique' => true])
-			->addIndex(['user_id'])
-			->create();
+        $this->table('articles')
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('slug', 'string', [
+                'default' => null,
+                'limit' => 191,
+                'null' => false,
+            ])
+            ->addColumn('body', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('published', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addIndex(['slug'], ['unique' => true])
+            ->addIndex(['user_id'])
+            ->create();
 
-		$this->table('article_tags', ['id' => false, 'primary_key' => ['article_id', 'tag_id']])
-			->addColumn('article_id', 'integer', [
-				'default' => null,
-				'limit' => 11,
-				'null' => false,
-			])
-			->addColumn('tag_id', 'integer', [
-				'default' => null,
-				'limit' => 11,
-				'null' => false,
-			])
-			->addIndex(['article_id'])
-			->addIndex(['tag_id'])
-			->create();
+        $this->table('article_tags', ['id' => false, 'primary_key' => ['article_id', 'tag_id']])
+            ->addColumn('article_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('tag_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addIndex(['article_id'])
+            ->addIndex(['tag_id'])
+            ->create();
 
-		$this->table('tags')
-			->addColumn('title', 'string', [
-				'default' => null,
-				'limit' => 191,
-				'null' => true,
-			])
-			->addColumn('created', 'datetime', [
-				'default' => null,
-				'limit' => null,
-				'null' => false,
-			])
-			->addColumn('modified', 'datetime', [
-				'default' => null,
-				'limit' => null,
-				'null' => false,
-			])
-			->addIndex(['title'], ['unique' => true])
-			->create();
+        $this->table('tags')
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 191,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addIndex(['title'], ['unique' => true])
+            ->create();
 
-		$this->table('articles')
-			->addForeignKey(
-				'user_id',
-				'users',
-				'id',
-				[
-					'update' => 'RESTRICT',
-					'delete' => 'RESTRICT',
-				]
-			)
-			->update();
+        $this->table('articles')
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'RESTRICT',
+                ]
+            )
+            ->update();
 
-		$this->table('article_tags')
-			->addForeignKey(
-				'article_id',
-				'articles',
-				'id',
-				[
-					'update' => 'RESTRICT',
-					'delete' => 'RESTRICT'
-				]
-			)
-			->addForeignKey(
-				'tag_id',
-				'tags',
-				'id',
-				[
-					'update' => 'RESTRICT',
-					'delete' => 'RESTRICT',
-				]
-			)
-			->update();
-	}
+        $this->table('article_tags')
+            ->addForeignKey(
+                'article_id',
+                'articles',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'RESTRICT'
+                ]
+            )
+            ->addForeignKey(
+                'tag_id',
+                'tags',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'RESTRICT',
+                ]
+            )
+            ->update();
+    }
 
-	public function down()
-	{
-		$this->table('articles')
-			->dropForeignKey('user_id');
+    /**
+     * down
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->table('articles')
+            ->dropForeignKey('user_id');
 
-		$this->table('article_tags')
-			->dropForeignKey('article_id')
-			->dropForeignKey('tag_id');
+        $this->table('article_tags')
+            ->dropForeignKey('article_id')
+            ->dropForeignKey('tag_id');
 
-		$this->table('users')->drop()->save();
-		$this->table('articles')->drop()->save();
-		$this->table('article_tags')->drop()->save();
-		$this->table('tags')->drop()->save();
-	}
+        $this->table('users')->drop()->save();
+        $this->table('articles')->drop()->save();
+        $this->table('article_tags')->drop()->save();
+        $this->table('tags')->drop()->save();
+    }
 }
