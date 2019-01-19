1f6e7b72b91e64a31e1a5606d91b2b4b10ff53c5
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-01-19 17:24:05 +0900

    create Controller/Api/ArticlesController class

---
+++ src/Controller/Api/ArticlesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Controller\AppController;
+use App\Model\Table\ArticlesTable;
+
+/**
+ * Class ArticlesController
+ * @package App\Controller\Api
+ * @property ArticlesTable Articles
+ */
+class ArticlesController extends AppController
+{
+}
