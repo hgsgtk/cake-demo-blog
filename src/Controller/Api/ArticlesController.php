9464b531161c95f86257e54ac0800e9c83134525
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-01-19 18:05:09 +0900

    passed test by temporary implementation

--- src/Controller/Api/ArticlesController.php
+++ src/Controller/Api/ArticlesController.php
@@ -31,7 +31,18 @@ class ArticlesController extends AppController
      */
     public function index()
     {
-        $articles = [];
+        // TODO: 記事一覧をデータベースから取得する
+        $articles = [
+            [
+                'user_id' => 1,
+                'title' => 'First Article',
+                'slug' => 'first',
+                'body' => 'First Article Body',
+                'published' => 1,
+                'created' => '2018-01-07 15:47:01',
+                'modified' => '2018-01-07 15:47:02',
+            ],
+        ];
         $this->set([
             '_serialize' => ['articles'],
             'articles' => $articles,
