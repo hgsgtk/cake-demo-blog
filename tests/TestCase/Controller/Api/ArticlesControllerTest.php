a2a4a2192d6ec9d6a2c7d8092ff1f582bc43709b
hgsgtk <hgsgtk@gmail.com>
2019-11-08 09:19:48 +0900

    Add a test testArticleDetailReturnArticleResource and failed

--- tests/TestCase/Controller/Api/ArticlesControllerTest.php
+++ tests/TestCase/Controller/Api/ArticlesControllerTest.php
@@ -25,4 +25,32 @@ final class ArticlesControllerTest extends TestCase
         // assertion
         $this->assertResponseSuccess();
     }
+
+    public function testArticleDetailReturnArticleResource(): void
+    {
+        // arrange
+        $this->configRequest([
+            'headers' => [
+                'Accept' => 'application/json',
+            ],
+        ]);
+
+        // act
+        $this->get('/api/articles/view/first');
+
+        // assertion
+        $expected = json_encode([
+            'article' => [
+                'id' => 1,
+                'user_id' => 1,
+                'title' => 'First Article',
+                'slug' => 'first',
+                'body' => 'First Article Body',
+                'published' => true,
+                'created' => '2018-01-07T15:47:01+00:00',
+                'modified' => '2018-01-07T15:47:02+00:00',
+            ],
+        ], JSON_PRETTY_PRINT);
+        $this->assertSame($expected, $this->_getBodyAsString());
+    }
 }
