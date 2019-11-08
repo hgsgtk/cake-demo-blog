8a37daa6dcd5e5ddd63c52d18732bf51040b5592
hgsgtk <hgsgtk@gmail.com>
2019-11-09 08:17:58 +0900

    Article details API returns article resource

--- src/Controller/Api/ArticlesController.php
+++ src/Controller/Api/ArticlesController.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 namespace App\Controller\Api;
 
 use App\Controller\AppController;
+use App\Model\Table\ArticlesTable;
 use Cake\View\JsonView;
 
+/**
+ * @property ArticlesTable Articles
+ */
 final class ArticlesController extends AppController
 {
     public function view($slug = null)
     {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+
         $this->viewBuilder()
             ->setClassName(JsonView::Class)
             ->setOption('serialize', 'response');
-        $this->set('response', []);
+        $this->set('response', ['article' => $article]);
     }
 }
--- tests/TestCase/Controller/Api/ArticlesControllerTest.php
+++ tests/TestCase/Controller/Api/ArticlesControllerTest.php
@@ -10,19 +10,52 @@ final class ArticlesControllerTest extends TestCase
 {
     use IntegrationTestTrait;
 
+    public $fixtures = [
+        'app.Articles',
+    ];
+
     public function testArticleDetailReturnSuccess(): void
     {
         // arrange
-        $this->configRequest([
-            'header' => [
-                'Accept' => 'application/json',
-            ],
-        ]);
+        $this->configJsonRequest();
 
         // act
         $this->get('/api/articles/view/first');
 
         // assertion
         $this->assertResponseSuccess();
+    }
+
+    public function testArticleDetailReturnArticleResource(): void
+    {
+        // arrange
+        $this->configJsonRequest();
+
+        // act
+        $this->get('/api/articles/view/first');
+
+        // assertion
+        $expected = json_encode([
+            'article' => [
+                'id' => 1,
+                'user_id' => 1,
+                'title' => 'First Article',
+                'slug' => 'first',
+                'body' => 'First Article Body',
+                'published' => true,
+                'created' => '2018-01-07T15:47:01+00:00',
+                'modified' => '2018-01-07T15:47:02+00:00',
+            ]
+        ], JSON_PRETTY_PRINT);
+        $this->assertJsonStringEqualsJsonString($expected, $this->_getBodyAsString());
+    }
+
+    private function configJsonRequest(): void
+    {
+        $this->configRequest([
+            'header' => [
+                'Accept' => 'application/json',
+            ],
+        ]);
     }
 }
