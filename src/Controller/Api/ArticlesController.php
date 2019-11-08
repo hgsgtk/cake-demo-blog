431c0dc1dc4a5a702785655f7e6d4152f21a1b48
hgsgtk <hgsgtk@gmail.com>
2019-11-08 09:20:07 +0900

    Make a little change and pass test

--- src/Controller/Api/ArticlesController.php
+++ src/Controller/Api/ArticlesController.php
@@ -9,9 +9,23 @@ final class ArticlesController extends AppController
 {
     public function view()
     {
+        // FIXME temporary implementation
+        $article = [
+            'id' => 1,
+            'user_id' => 1,
+            'title' => 'First Article',
+            'slug' => 'first',
+            'body' => 'First Article Body',
+            'published' => true,
+            'created' => '2018-01-07T15:47:01+00:00',
+            'modified' => '2018-01-07T15:47:02+00:00',
+        ];
+
         $this->viewBuilder()
             ->setClassName('Json')
             ->setOption('serialize', 'response');
-        $this->set(['response' => []]);
+        $this->set(['response' => [
+            'article' => $article,
+        ]]);
     }
 }
